a4be7c0d31b1a3bc2132ccbe5462a3fd75361b8e
c4vxl <141231805+c4vxl@users.noreply.github.com>
2024-08-02 22:26:32 +0200

    Update dataset_preperation.py

--- dataset_preperation.py
+++ dataset_preperation.py
@@ -10,43 +10,34 @@ import PIL.Image as Image
 import os
 import json
 
-def convert_to_black_white(file_path):
-    image = Image.open(file_path)
 
-    if f"{image.width}x{image.height}" != "84x96":
-        print(f"Resizing {file_path}...")
-        image = image.resize((84, 96))
-    
-    pixels = [(x, y) for y in range(0, image.height) for x in range(0, image.width)]
-    for pixel in pixels:
-        image.putpixel(pixel, (255, 255, 255) if image.getpixel(pixel)[0] >= 200 else 0)
-    
-    image.save(file_path)
-    image.close()
+def load_data_batch(image, num):
+    pixels = list([0 if x[0] >= 200 else 1 for x in image.getdata()])
+
+    return {
+        "num": num,
+        "pixels": pixels
+    }
 
 dataset = []
 
-for i in range(0, 10):
-    folder = f"numbers/{i}/"
-    print(f"Files of {i}")
+for d in range(1, 30):
+    for i in range(10):
+        folder = f"data/{d}/{i}/"
+        print(f"dir: {folder}")
 
-    for file in os.scandir(folder):
-        file_path = folder + file.name
+        for file in os.scandir(folder):
+            file = file.name
+            file = folder + file
 
-        convert_to_black_white(file_path)
+            print("Image: \t", file)
 
-        image = Image.open(file_path)
+            image = Image.open(file).convert("RGB")
+            image = image.resize((84, 96))
 
-        pixels = list([0 if x[0] >= 200 else 1 for x in image.getdata()])
+            data = load_data_batch(image, i)
 
-        data = {
-            "num": i,
-            "pixels": pixels
-        }
-
-        dataset.append(data)
-
-
+            dataset.append(data)
 
 
 open("dataset.json", "w").write(json.dumps(dataset))
